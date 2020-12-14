792fd0a9091d71e9013a42b9a359df6e2d35eda3
inazarm <inazar.m@gmail.com>
2020-12-14 16:30:53 +0500

    display binary image on view in mvc

--- DisplayBinarySavedIamge/Controllers/HomeController.cs
+++ DisplayBinarySavedIamge/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DisplayBinarySavedIamge.Models;
 
 namespace DisplayBinarySavedIamge.Controllers
 {
     public class HomeController : Controller
     {
+        CAPSEntities db = new CAPSEntities();
         public ActionResult Index()
         {
+            List<tblStudent> list = new List<tblStudent>();
+            list= db.tblStudents.Where(x=>x.RegistrationNo==2305).ToList();
+            foreach (var item in list)
+            {
+                ViewBag.img = item.Picture;
+            }
+            
             return View();
         }
 
